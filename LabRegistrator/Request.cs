968c4e2dbb32eb3bcd70595f9c711d628af2d021
SPBHELIX\bolshov.e <bolshov.e@spb.helix.ru>
2017-08-04 13:13:52 +0300

    Вынужденный перенос репозитория, теперь работаем тут.

---
+++ LabRegistrator/Request.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRegistrator
+{
+    class Request
+    {
+       private string url = "https://api.medlinx.online";
+
+    }
+}
---
+++ LabRegistrator/Token.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabRegistrator
+{
+    class Token
+    {
+        private string _token;
+
+        public string Value
+        {
+           get { return _token; }
+           set { this._token = value; }
+        }
+    }
+}
